a4f4019314c204cbc7e2153e7cc804fac3729bce
nandan1234 <36558666+nandan1234@users.noreply.github.com>
2020-03-19 14:38:51 +0530

    Update openidconnect.inc.php

--- auth-openidconnect-w3-master/cfg/openidconnect.inc.php
+++ auth-openidconnect-w3-master/cfg/openidconnect.inc.php
@@ -3,7 +3,7 @@
 	$config_openidconnect->authorize_url = "https://w3id.alpha.sso.ibm.com/isam/oidc/endpoint/amapp-runtime-oidcidp/authorize";
 	$config_openidconnect->token_url = "https://w3id.alpha.sso.ibm.com/isam/oidc/endpoint/amapp-runtime-oidcidp/token";
 	$config_openidconnect->introspect_url = "https://w3id.alpha.sso.ibm.com/isam/oidc/endpoint/amapp-runtime-oidcidp/introspect";
-	$config_openidconnect->client_id = NDViZDIzY2QtOTU1Ny00;
-	$config_openidconnect->client_secret = NDFlMzIxODMtZTU0YS00;
+	$config_openidconnect->client_id = "NDViZDIzY2QtOTU1Ny00";
+	$config_openidconnect->client_secret = "NDFlMzIxODMtZTU0YS00";
 	$config_openidconnect->redirect_url = "https://localhost:5000/parquet";
 ?>
