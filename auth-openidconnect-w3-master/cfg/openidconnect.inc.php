38f89c575759a3fa0677ebee225b4dfb727fbb9d
nandan1234 <36558666+nandan1234@users.noreply.github.com>
2020-03-19 13:48:28 +0530

    Update openidconnect.inc.php

--- auth-openidconnect-w3-master/cfg/openidconnect.inc.php
+++ auth-openidconnect-w3-master/cfg/openidconnect.inc.php
@@ -1,9 +1,9 @@
 <?php
 	$config_openidconnect = new stdClass();
-	$config_openidconnect->authorize_url = "https://w3id.sso.ibm.com/isam/oidc/endpoint/amapp-runtime-oidcidp/authorize";
-	$config_openidconnect->token_url = "https://w3id.sso.ibm.com/isam/oidc/endpoint/amapp-runtime-oidcidp/token";
-	$config_openidconnect->introspect_url = "https://w3id.sso.ibm.com/isam/oidc/endpoint/amapp-runtime-oidcidp/introspect";
-	$config_openidconnect->client_id = NTI1MzQ5MmMtZjA3Ny00;
-	$config_openidconnect->client_secret = OTExMzQzYTctOTQwNS00;
-	$config_openidconnect->redirect_url = "https://";
+	$config_openidconnect->authorize_url = "https://w3id.alpha.sso.ibm.com/isam/oidc/endpoint/amapp-runtime-oidcidp/authorize";
+	$config_openidconnect->token_url = "https://w3id.alpha.sso.ibm.com/isam/oidc/endpoint/amapp-runtime-oidcidp/token";
+	$config_openidconnect->introspect_url = "https://w3id.alpha.sso.ibm.com/isam/oidc/endpoint/amapp-runtime-oidcidp/introspect";
+	$config_openidconnect->client_id = NDViZDIzY2QtOTU1Ny00;
+	$config_openidconnect->client_secret = NDFlMzIxODMtZTU0YS00;
+	$config_openidconnect->redirect_url = "https://localhost:5000/parquet";
 ?>
