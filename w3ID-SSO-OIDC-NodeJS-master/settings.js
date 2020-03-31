5ca0bd6aa41c647f1d11ac64ec1ffde270cd85ff
nandan1234 <36558666+nandan1234@users.noreply.github.com>
2020-03-31 12:00:48 +0530

    Update settings.js

--- w3ID-SSO-OIDC-NodeJS-master/settings.js
+++ w3ID-SSO-OIDC-NodeJS-master/settings.js
@@ -1,6 +1,6 @@
 exports.client_id="NDViZDIzY2QtOTU1Ny00";
 exports.client_secret="NDFlMzIxODMtZTU0YS00";
-exports.callback_url="https://localhost:5000/parquet";
+exports.callback_url="https://0.0.0.0:8081/parquet";
 exports.authorization_url="https://w3id.alpha.sso.ibm.com/isam/oidc/endpoint/amapp-runtime-oidcidp/authorize";
 exports.token_url="https://w3id.alpha.sso.ibm.com/isam/oidc/endpoint/amapp-runtime-oidcidp/token";
 exports.issuer_id="https://w3id.alpha.sso.ibm.com/isam";
